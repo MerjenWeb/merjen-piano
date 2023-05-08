f81efe699c0e0f9df041d953da5cab59b549d7a1
Merjen <merjengarajaeva@yandex.ru>
2023-05-08 16:36:16 +0500

    Piano web page

---
+++ script.js
@@ -0,0 +1,25 @@
+'use strict';
+const keys = document.querySelectorAll('.key');
+const playSound = function (key) {
+  const sound = new Audio(`/keys/${key}.mp3`);
+  sound.play();
+};
+
+//CLICKING
+keys.forEach(function (key) {
+  key.addEventListener('click', function () {
+    const keyHTMLtext = key.innerText;
+    playSound(keyHTMLtext);
+  });
+});
+
+//KEYDOWN
+document.addEventListener('keydown', function (e) {
+  keys.forEach(function (key) {
+    const keyHTMLtext = key.innerText;
+    const pressedKey = e.key;
+    if (pressedKey === keyHTMLtext) {
+      playSound(keyHTMLtext);
+    }
+  });
+});
